5ba5fbe3270c1a7b8a78474c6b2648a23506dacf
ignatKupryashin <ikupryashin@gmail.com>
2022-10-16 16:27:00 +0300

    написал модуль start. Пока без проверки валидности

--- start.py
+++ start.py
@@ -1,4 +1,12 @@
-# Функционал:
+from modules.ui import ui
 # 1) Проверка наличия файла database.csv (если нет - создать файл database.csv)
 # 2) Проверка файла database.csv на валидность (пока хз как организовать). Мб, если не валидный - создать файл database1 или что то такое. Тут нужно подумать
 # 3) Если всё норм - запустить файл modules/ui.py
+print("Телефонный справочник запущен")
+try:
+    file = open("database.csv")
+except:
+    file = open("database.csv", "w+")
+    file.close()
+
+ui()
